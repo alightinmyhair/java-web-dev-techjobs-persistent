3b0875fae389430fc9b7da1e0ee2ddf386d54962
rkuhl <alightinmyhair@gmail.com>
2020-02-09 17:31:02 -0600

    Finished Part 1 and 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,20 @@
 ## Part 1: Test it with SQL
 
+Columns & Data Types: id (int), employer (varchar(255)), name (varchar(255)), skills (varchar(255))
+
 ## Part 2: Test it with SQL
+
+Two Options:
+select *
+from employer
+where location like '%St. Louis City%'
+
+OR
+
+select *
+from employer
+where location ='St. Louis City'
+
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min = 3, max = 100, message = "Description must be between 3 and 100 characters")
+    private String description;
+
+    public Skill(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
